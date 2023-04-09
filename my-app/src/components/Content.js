e689c68c15af00d7eb34ea57ede58b6e5f706e6c
kshamse <khubab220@gmail.com>
2023-04-09 04:19:37 +0100

    Add loader component and the component did mount function

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 import css from './css/Content.module.css'
 import { savedPosts } from "../posts.json"
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isLoaded: false
+        }
+    }
 
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState(()=>({
+                isLoaded: true
+            }))
+        }, 2000);
     }
 
     render() {
@@ -16,8 +27,13 @@ export class Content extends Component {
                     <h1>My Photos</h1>
                 </div>
                 <div className='css.SearchResults'>
-                    <PostItem posts={savedPosts} />
+                    {
+                    this.state.isLoaded? 
+                    <PostItem posts={savedPosts} />: 
+                    <Loader/>
+                    }
                 </div>
+                
             </div>
         )
     }
---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import css from './css/Loader.module.css'
+
+export default function Loader() {
+    return (
+        <div className={css.Loader}>
+        </div>
+    )
+}
