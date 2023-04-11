6903b0f43cad90bbab030e79ef1a683370433d98
kshamse <khubab220@gmail.com>
2023-04-11 02:18:23 +0100

    Add search input and filter posts based on it

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -8,16 +8,25 @@ export class Content extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: []
         }
     }
 
     componentDidMount(){
         setTimeout(() => {
             this.setState(()=>({
-                isLoaded: true
+                isLoaded: true,
+                posts: savedPosts
             }))
         }, 2000);
+    }
+
+    searchPosts = (event) => {
+        const postName = event.target.value.toLowerCase()
+        this.setState(()=>({
+            posts: savedPosts.filter((post) => post.name.toLowerCase().includes(postName))
+        }))
     }
 
     render() {
@@ -25,11 +34,19 @@ export class Content extends Component {
             <div className={css.Content}>
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
+                    <form>
+                        <lablel htmlFor='search'>Search:</lablel>
+                        <input 
+                        name='searchInput' 
+                        id='searchInput'
+                        onChange={(event) => this.searchPosts(event)} />
+                    </form>
+                    <h4>posts found: {this.state.posts.length}</h4>
                 </div>
                 <div className={css.SearchResults}>
                     {
                     this.state.isLoaded? 
-                    <PostItem posts={savedPosts} />: 
+                    <PostItem posts={this.state.posts} />: 
                     <Loader/>
                     }
                 </div>
